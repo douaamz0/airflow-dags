854b66f28ac28b5c4784437b42ed86add9e9ccd4
Douaa Mzaouir <douaa.mzaouir@intelcia.com>
2025-03-24 14:43:48 +0000

    new dag

---
+++ dags/example_dag.py
@@ -0,0 +1,12 @@
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='example_dag',
+    start_date=datetime(2025, 1, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    task = DummyOperator(task_id='dummy_task')
